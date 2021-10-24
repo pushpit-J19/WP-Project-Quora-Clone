b084623200bf5a841ecde8f919f3cf132a108575
pushpit-J19 <54130460+pushpit-J19@users.noreply.github.com>
2021-10-24 22:46:31 +0530

    sdvsd

--- Components/profile.php
+++ Components/profile.php
@@ -152,17 +152,17 @@
                             <div class="d-flex row">
                                 <div>
                                     <h6>Credentials & Highlights</h6>
-                                    <button class="rounded-circle edit">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
-                                        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
-                                    </svg>
+                                    <button class="rounded-circle edit" onclick="document.getElementById('cred-update').style.display = 'flex';" onclick="cred()">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
+                                            <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                                        </svg>
                                     </button>
                                 </div>
                             </div>
                             <div class="d-flex row"><hr></div>
                             
-                            <div class="d-flex row">
-                                <span style="border-radius:50%; background-color:#eee;" class="px-1 mr-2 my-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-workspace" viewBox="0 0 16 16">
+                            <div class="d-flex row ">
+                                <span style="border-radius:50%; background-color:#eee;" class="px-1 mr-2 my-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-workspace " viewBox="0 0 16 16">
                                 <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" fill="#000"/>
                                 <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z" fill="#000"/>
                                 </svg> </span>
@@ -226,14 +226,38 @@
                             </div>
                         </div>
                     </div>
-
-                    
-                    
                 </div>
 
 
             </div>
         </main>
+
+        <div id="cred-container" class="qcardcontainer bg-dark">
+            <div class="overlay d-flex justify-content-center align-items-center">
+                
+            
+
+                <div class="card w-50 px-5 py-4" id="cred-update">
+                    <form>
+                        <div class="form-group">
+                            <label for="work">Works at</label>
+                            <input type="text" class="form-control" id="work" >
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="study">Study at</label>
+                            <input type="text" class="form-control" id="study" >
+                        </div>
+                        
+                        <div class="form-group">
+                            <label for="live">Lives at</label>
+                            <input type="text" class="form-control" id="live" >
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
 
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -286,6 +310,11 @@
                 document.getElementById("changeprofile").click();
                 console.log("h1");
             };
+
+            function cred(){
+                
+            }
+
         </script>
 
 
