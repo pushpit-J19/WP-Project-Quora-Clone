ea97fa3319749ccfdc0581793877b3fabb4508ee
pushpit-J19 <54130460+pushpit-J19@users.noreply.github.com>
2021-10-26 15:15:02 +0530

    cleaned

--- Components/credentials.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define('EMAILID', 'pset8.homepage@gmail.com');
-    define('PASSWORD', 'tempuserpassword1212');
-    define('EMAIL_SENDER', 'quoracloneteam@gmail.com');
-    define('PASSWORD_SENDER', 'quoraclonepassword1212');
-?>
